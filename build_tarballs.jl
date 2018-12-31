75bd0134158b5129a3b2c1524f9f584169b746e0
tshort <tshort.rlists@gmail.com>
2018-12-30 21:10:15 -0500

    Update for Emscriptn shard changes

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -6,8 +6,6 @@ version = v"0.5.1"
 ENV["BINARYBUILDER_USE_CCACHE"] = "false"
 
 sources = [
-    # "https://github.com/flame/blis/archive/0.3.0.tar.gz" =>
-    # "d34d17df7bdc2be8771fe0b7f867109fd10437ac91e2a29000a4a23164c7f0da",
     "https://github.com/flame/blis/archive/0.5.1.tar.gz" =>
     "7816a1f6085b1d779f074bca2435c195c524582edbb04d9cd399dade9187a72d",
 ]
@@ -20,8 +18,8 @@ cd $WORKSPACE/srcdir/blis
 ./configure -p $prefix generic
 
 if [[ ${target} == wasm32-* ]]; then
-    apk add nodejs
-    emcc -v
+    # apk add nodejs
+    # emcc -v
     make -j${nproc} CC_VENDOR=clang
     make install CC_VENDOR=clang
 else 
